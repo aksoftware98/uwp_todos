345ae3bb2eb22fe197703c14e0c9c5fe34053ebc
Ahmad Mozaffar <35115273+aksoftware98@users.noreply.github.com>
2020-09-22 06:48:41 +0300

    Update Credentials

--- Project/ToDos.Api/Startup.cs
+++ Project/ToDos.Api/Startup.cs
@@ -35,7 +35,7 @@ namespace ToDos.Api
 
             services.AddDbContext<ApplicationDbContext>(options =>
             {
-                options.UseCosmos("https://localhost:8081", "C2y6yDjf5/R+ob0N8A7Cgv30VRDJIWEHLM+4QDU5DE2nQ9nDuVTqobD4b8mGGyPMbIZnqyMsEcaGQy67XIw/Jw==", "ToDos");
+                options.UseCosmos("{YOUR_COSMOS_DB_ENDPOINT}", "{YOUR_COSMOS_DB_KEY}", "ToDos");
             });
 
             services.AddScoped<IItemsRepository, ItemsRepository>();
